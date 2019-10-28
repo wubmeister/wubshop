e785fe923ebf5f10df79900845664c1ea05961b9
Wubbo Bos <wubbo@addnoise.nl>
2019-10-28 15:16:18 +0100

    Add classes for forms

---
+++ src/Form/Field/Field.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form\Field;
+
+class Field
+{
+    protected $name;
+    protected $value;
+    protected $isArray;
+    protected $parent;
+
+    public function __construct($name, array $options = [])
+    {
+        $this->name = $name;
+        if (isset($options["is_array"])) $this->isArray = (bool)$options["is_array"];
+    }
+
+    public function setValue($value, $validate = false)
+    {
+        $this->value = $value;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    public function getFullName()
+    {
+        if ($this->parent) {
+            $name = $this->parent->getFullName() . '[' . $this->name . ']';
+        } else {
+            $name = $this->name;
+        }
+        if ($this->isArray) {
+            $name .= '[]';
+        }
+
+        return $name;
+    }
+
+    public function getId()
+    {
+        if ($this->parent) {
+            $id = $this->parent->getid() . '_' . $this->name;
+        } else {
+            $id = $this->name;
+        }
+        return $id;
+    }
+}
---
+++ src/Form/Form.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Form\Field\Field;
+
+class Form
+{
+    protected $fields = [];
+    protected $fieldOrder = [];
+
+    public function addField(Field $field)
+    {
+        $name = $field->getName();
+        $this->fieldOrder[] = $name;
+        $this->fields[$name] = $field;
+    }
+
+    public function getFields()
+    {
+        $fields = [];
+        foreach ($this->fieldOrder as $name) {
+            $fields[] = $this->fields[$name];
+        }
+        return $fields;
+    }
+
+    public function setValues(array $values, $validate = false)
+    {
+        foreach ($values as $key => $value) {
+            if (isset($this->fields[$key])) {
+                $this->fields[$key]->setValue($value, $validate);
+            }
+        }
+    }
+
+    public function getValues()
+    {
+        $values = [];
+        foreach ($this->fields as $name => $field) {
+            $values[$name] = $field->getValue();
+        }
+
+        return $values;
+    }
+
+    public function getField($name)
+    {
+        return isset($this->fields[$name]) ? $this->fields[$name] : null;
+    }
+}
