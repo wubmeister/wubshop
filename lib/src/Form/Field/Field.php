99d192b0fa96b76b03a1b6cc62a093b69c12cde3
Wubbo Bos <wubbo@addnoise.nl>
2019-12-13 13:38:39 +0100

    Test and document Form Field

--- lib/src/Form/Field/Field.php
+++ lib/src/Form/Field/Field.php
@@ -2,18 +2,46 @@
 
 namespace Lib\Form\Field;
 
+/**
+ * Class to represent a form field
+ *
+ * @author Wubbo Bos
+ */
 class Field
 {
+    /** @var string $name The name without any brackets */
     protected $name;
+
+    /** @var mixed $value */
     protected $value;
-    protected $isArray;
+
+    /** @var bool $isArray Flag to determine where this field holds an array of values */
+    protected $isArray = false;
+
+    /** @var bool $required */
     protected $required = false;
+
+    /** @var Field $parent */
     protected $parent;
+
+    /** @var bool $valid */
     protected $valid = true;
+
+    /** @var bool $changed */
     protected $changed = false;
+
+    /** @var array $errors */
     protected $errors = [];
+
+    /** @var array $inputFilters */
     protected $inputFilters = [];
 
+    /**
+     * Constructor
+     *
+     * @param string $name The field name without any brackets
+     * @param array $options Optional.
+     */
     public function __construct($name, array $options = [])
     {
         $this->name = $name;
@@ -21,11 +49,22 @@ class Field
         if (isset($options["required"])) $this->required = (bool)$options["required"];
     }
 
+    /**
+     * Returns the field name
+     *
+     * @return string
+     */
     public function getName()
     {
         return $this->name;
     }
 
+    /**
+     * Sets the value for the field
+     *
+     * @param mixed $value
+     * @param bool $filter Pass TRUE or omit to filter the value
+     */
     public function setValue($value, $filter = false)
     {
         $this->value = $value;
@@ -33,16 +72,29 @@ class Field
         if ($filter) $this->filter();
     }
 
+    /**
+     * Returns the current value of the field
+     *
+     * @return mixed
+     */
     public function getValue()
     {
         return $this->value;
     }
 
+    /**
+     * Method to check if this field is required
+     *
+     * @return bool
+     */
     public function isRequired()
     {
         return $this->required;
     }
 
+    /**
+     * Filters the current value of the field
+     */
     protected function filter()
     {
         if ($this->required && empty($this->value)) {
@@ -63,6 +115,11 @@ class Field
         $this->value = $value;
     }
 
+    /**
+     * Method to check if this field value is valid
+     *
+     * @return bool
+     */
     public function isValid()
     {
         if ($this->valid && $this->changed && $this->required && empty($this->value)) {
@@ -72,11 +129,21 @@ class Field
         return $this->valid;
     }
 
+    /**
+     * Returns any validation error messages
+     *
+     * @return array
+     */
     public function getErrors()
     {
         return $this->errors;
     }
 
+    /**
+     * Returns the full name for the field to be used in the 'name' attribute in HTML
+     *
+     * @return string
+     */
     public function getFullName()
     {
         if ($this->parent) {
@@ -91,6 +158,11 @@ class Field
         return $name;
     }
 
+    /**
+     * Returns a unique ID for the field to be used in the 'id' attribute in HTML
+     *
+     * @return string
+     */
     public function getId()
     {
         if ($this->parent) {
---
+++ lib/tests/Form/Field/FieldTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use Lib\Form\Field\Field;
+use PHPUnit\Framework\TestCase;
+
+final class App_Field_FieldTest extends TestCase
+{
+    public function testCanSetValue()
+    {
+        $field = new Field("fieldname");
+        $field->setValue("123");
+        $this->assertEquals("123", $field->getValue());
+    }
+
+    public function testCanBeRequired()
+    {
+        $field = new Field("fieldname");
+        $this->assertFalse($field->isRequired());
+
+        $field = new Field("fieldname", [ "required" => true ]);
+        $this->assertTrue($field->isRequired());
+    }
+
+    public function testCanGetNameAndId()
+    {
+        $field = new Field("fieldname");
+
+        $this->assertEquals("fieldname", $field->getName());
+        $this->assertEquals("fieldname", $field->getFullName());
+        $this->assertEquals("fieldname", $field->getId());
+    }
+}
